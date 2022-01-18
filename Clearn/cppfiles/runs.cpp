069c789250f67e11f3ff4a12f5b50a6b21b4b34a
cutecwc <cwcsocute@gmail.com>
2022-01-18 09:24:19 +0800

    0118_2022v2

--- Clearn/cppfiles/runs.cpp
+++ Clearn/cppfiles/runs.cpp
@@ -1,12 +1,15 @@
 # include<iostream>
 # include<windows.h>
-# include"1.cpp"
+
+# include"MyTypes.h"
 
 int main(void){
+    using namespace std;
     //在这里放入测试函数的名称
-    Timus a;
-    a.p1_pr2();
-
+    Myarray<int> c;
+    c.push_back(1);
+    c.push_back(2);
+    c.print();
     system("Pause");
     return 0;
 }

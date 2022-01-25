c8067514465d297f54fbefd4e1531ce901a0b273
cutecwc <cwcsocute@gmail.com>
2022-01-25 08:22:14 +0800

    0125_2022v1

--- Amacrs/Amacrs/Timus1.h
+++ Amacrs/Amacrs/Timus1.h
@@ -3,12 +3,21 @@
 
 #include<iostream>
 #include<locale>
+#include<Windows.h>
 using namespace std;
 
 class Mc1{
 public:
 	void test1();
+	void test2();
+	void test3();
+	void test4();
 };
+
+void timu1() {
+	Mc1 c;
+	c.test3();
+}
 
 void Mc1::test1() {
 	locale china("chs");
@@ -17,19 +26,33 @@ void Mc1::test1() {
 	return;
 }
 
-/*
-	locale china("chs");//use china character
-	wcin.imbue(china);//use locale object
-	wcout.imbue(china);
-	wstring s;
-	wchar_t wc=L'。';//wide character,wide string may be L"宽字符"
-	while (getline(wcin,s,wc))
-	{
-		wcout<<s<<endl;
-	}
-*/
-void timu1() {
-	Mc1 c;
-	c.test1();
+void Mc1::test2(){
+	cout << "id""en1" << endl;
+	cout << "id\
+en2" << endl;
+	cout << "iden3" << endl;
+	return;
 }
+
+void Mc1::test3()
+{
+	string cc = "1234567890";
+	int len = cc.size();
+	cout << "len is " << len << endl;
+	cout << cc;
+	for (int i = 0; i < len; i++) {
+		Sleep(300);
+		cout << " ";
+		cout << "\b\b";
+	}
+}
+
+inline void Mc1::test4()
+{
+	int i = 4, j, k;
+	j = i--;    //i先赋值给j，然后i在执行i=i-1;
+	k = --i;    //i先执行i=i-1，然后再赋值给;
+	printf("i=%d,j=%d,k=%d", i, j, k);
+}
+
 #endif
